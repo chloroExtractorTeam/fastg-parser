09bb602ae58c2c8fb34f20b5481875e22ae9ca44
Frank Förster <frank.foerster@biozentrum.uni-wuerzburg.de>
2016-10-12 20:09:44 +0200

    Get the nodenames and estimate basename

--- import.pl
+++ import.pl
@@ -14,6 +14,17 @@ open(FH, "<", $infile) || die "Unable to open file '$infile'\n";
 while (<FH>)
 {
     next unless (/^>/);
+
+    # extract Node information
+    while ($_ =~ /(NODE_[^:;,]+)/g)
+    {
+
+	my $nodename = $1;
+	# estimate node basename
+
+	my $nodebasename = $nodename;
+	$nodename=~s/'$//; # delete a ' if necessary
+    }
 }
 
 close(FH) || die "Unable to close file '$infile'\n";
