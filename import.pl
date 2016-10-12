1d9ac02cd3a0756ea091327fd5b040653ff79168
Frank Förster <frank.foerster@biozentrum.uni-wuerzburg.de>
2016-10-12 20:05:43 +0200

    Prepared directed graph

--- import.pl
+++ import.pl
@@ -3,17 +3,17 @@
 use strict;
 use warnings;
 
-use Graph;
+use Graph::Directed;
 
 my $infile = $ARGV[0];
+
+my $g = Graph::Directed->new;
 
 open(FH, "<", $infile) || die "Unable to open file '$infile'\n";
 
 while (<FH>)
 {
     next unless (/^>/);
-
-    
 }
 
 close(FH) || die "Unable to close file '$infile'\n";
