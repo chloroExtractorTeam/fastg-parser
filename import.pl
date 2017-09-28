1a02fa701f23ce9fd33aa35bffd2ce622506dbaf
Frank Förster <foersterfrank@gmx.de>
2017-09-28 02:01:03 +0200

    Bump to version v0.3.4

--- import.pl
+++ import.pl
@@ -44,7 +44,7 @@ my $MINSEQLEN = 25000;
 my $MAXSEQLEN = 1000000;
 my $FACTOR4RESCUE = 10;
 
-use version 0.77; our $VERSION = version->declare("v0.3.3");
+use version 0.77; our $VERSION = version->declare("v0.3.4");
 
 our $ID = 'fcg';
 
